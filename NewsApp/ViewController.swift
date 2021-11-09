1b03608075ed01ae25f913df5222312e66c0e9f4
mincheol <homerjay@naver.com>
2021-11-10 06:38:16 +0900

    ㄴㄴ

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -5,6 +5,11 @@
 //  Created by 申民鐡 on 2021/11/09.
 //
 
+
+//1. http 통신
+//2. api 데이터 형테는 json 이다 {"돈":"1000"} 키밸류값 {["10","200","3000"]}
+
+
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
@@ -16,7 +21,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        //dequeueReusablecell은 테이블 셀을 만들고 그것을 지정한다.
         let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
         cell.LabelText.text = "이것은 \(indexPath.row) 번째 입니다."
         return cell
@@ -25,7 +30,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     //click
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(indexPath)번째 클릭입니다.")
+        print("\(indexPath.row)번째 클릭입니다.")
     }
 
     override func viewDidLoad() {
