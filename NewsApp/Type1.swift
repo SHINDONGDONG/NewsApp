55ebbad0718b095d0ed04ebeca2f68b2ca54820c
mincheol <homerjay@naver.com>
2021-11-10 06:19:20 +0900

    news app]

---
+++ NewsApp/Type1.swift
@@ -0,0 +1,15 @@
+//
+//  Type1.swift
+//  NewsApp
+//
+//  Created by 申民鐡 on 2021/11/09.
+//
+
+import UIKit
+
+class Type1 : UITableViewCell {
+    
+    
+    @IBOutlet weak var LabelText: UILabel!
+    
+}
--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    @IBOutlet weak var TableViewMain: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        99
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
+        cell.LabelText.text = "이것은 \(indexPath.row) 번째 입니다."
+        return cell
+    }
+    
+    
+    //click
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath)번째 클릭입니다.")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TableViewMain.delegate = self
+        TableViewMain.dataSource = self
+        
+        
     }
 
 
